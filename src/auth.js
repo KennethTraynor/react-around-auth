f92a8842d177a73fa9366a303e552c2b63311c73
Kenneth Traynor <cyannotive@gmail.com>
2021-07-15 03:46:31 -0400

    feat: add initial auth functions

---
+++ src/auth.js
@@ -0,0 +1,57 @@
+const baseUrl = 'https://register.nomoreparties.co';
+
+export const register = (password, email) => {
+    return fetch(`${baseUrl}/signup`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ password, email })
+    })
+        .then((response) => {
+            try {
+                if (response.status === 200) {
+                    return response.json();
+                }
+            } catch (err) {
+                return (err)
+            }
+        })
+        .then((res) => {
+            return res;
+        })
+        .catch((err) => console.log(err));
+};
+
+export const authorize = (password, email) => {
+    return fetch(`${baseUrl}/signin`, {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ password, email })
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.token) {
+                localStorage.setItem("token", data.token);
+                return data;
+            } else {
+                return;
+            }
+        })
+        .catch((err) => console.log(err));
+};
+
+export const checkToken = (token) => {
+    return fetch(`${baseUrl}/users/me`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        }
+    })
+        .then(res => res.json())
+        .then(data => data)
+        .catch((err) => console.log(err));
+}
