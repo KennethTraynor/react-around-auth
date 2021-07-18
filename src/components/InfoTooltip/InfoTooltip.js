b91dab727a5c8eb134fe1d0e3e7a9fbd4b1d6607
Kenneth Traynor <cyannotive@gmail.com>
2021-07-18 02:27:15 -0400

    feat: add info-tooltip responsive css

--- src/components/InfoTooltip/InfoTooltip.js
+++ src/components/InfoTooltip/InfoTooltip.js
@@ -4,7 +4,7 @@ function InfoTooltip({ message, isOpen, onClose, onPopupBackgroundClick, iconSta
 
     return (
         <div className={`popup popup_type_info ${isOpen ? 'popup_opened' : ''}`} onClick={onPopupBackgroundClick}>
-            <div className='popup__container'>
+            <div className='popup__container popup__container_type_info'>
                 <button className='popup__close-button popup__close-button_type_info' aria-label='close' name='close' type='button' onClick={onClose} ></button>
                 <div className='info-tooltip'>
                     <div className={`info-tooltip__icon info-tooltip__icon_type_${iconStatus}`}></div>
