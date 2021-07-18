9714b54330bcd6709ed88326b907dc2aeebb0c09
Kenneth Traynor <cyannotive@gmail.com>
2021-07-18 06:20:15 -0400

    feat: add header mobile css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,46 @@
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 import logoPath from '../../images/logo.svg';
 
 function Header({ loggedIn, email, onSignOut, currentPageType }) {
 
+    const [isAboveInfoVisible, setAboveInfoVisible] = React.useState(false);
+
+    useEffect(() => {
+        if (!loggedIn) {
+            setAboveInfoVisible(false);
+        }
+    }, [loggedIn])
+
+    const handleMenuButtonClick = () => {
+        setAboveInfoVisible(!isAboveInfoVisible);
+    }
+
     return (
         <header className='header'>
-            <img src={logoPath} className='header__logo' alt='Around The U.S. logo' />
-            <div className='header__info-container'>
-                {loggedIn && <p className='header__email'>{email}</p>}
-
-                {loggedIn && <button className='header__user-state-button header__user-state-button_type_logout' onClick={onSignOut}>Log Out</button>}
-                {!loggedIn && currentPageType === 'signup' && <Link className='header__user-state-button header__user-state-button_type_signin' to='/signin'>Log in</Link>}
-                {!loggedIn && currentPageType === 'signin' && <Link className='header__user-state-button header__user-state-button_type_signin' to='/signup'>Sign Up</Link>}
-                
+            {loggedIn &&
+                <div className={`header__above-info ${isAboveInfoVisible ? 'header__above-info_visible' : ''}`}>
+                    <p className='header__email header__email_type_above'>{email}</p>
+                    <button className='header__logout-button header__logout-button_type_above' onClick={onSignOut}>Log Out</button>
+                </div>
+            }
+            <div className='header__container'>
+                <img src={logoPath} className='header__logo' alt='Around The U.S. logo' />
+                <div className='header__info-container'>
+                    {loggedIn &&
+                        <>
+                            <p className='header__email header__email_type_bar'>{email}</p>
+                            <button className='header__logout-button header__logout-button_type_bar' onClick={onSignOut}>Log Out</button>
+                            <button className={`header__menu-button ${isAboveInfoVisible ? 'header__menu-button_active' : ''}`} onClick={handleMenuButtonClick}></button>
+                        </>
+                    }
+                    {!loggedIn &&
+                        <>
+                            {currentPageType === 'signup' && <Link className='header__login-signup-link' to='/signin'>Log in</Link>}
+                            {currentPageType === 'signin' && <Link className='header__login-signup-link' to='/signup'>Sign Up</Link>}
+                        </>
+                    }
+                </div>
             </div>
         </header>
     )
